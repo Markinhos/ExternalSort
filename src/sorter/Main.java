83a7059448205b1a5147a928bf54797ef357837a
Markinhos <marcos.arranz.mateo@gmail.com>
2016-01-07 02:17:42 +0100

    remove fixed arguments in main

--- src/sorter/Main.java
+++ src/sorter/Main.java
@@ -3,6 +3,6 @@ package sorter;
 public class Main {
 
 	public static void main(String[] args){
-		Sorter.externalSort("/tmp/bigfile.txt", "/tmp/output.txt");
+		Sorter.externalSort(args[0], args[1]);
 	}
 }
